b2f09ac4ce8124ee21cc23c8a61d9d822ed473bb
kandlly <kandlly10000@gmail.com>
2020-09-11 00:31:39 +0900

    Create backend project.

---
+++ reactSpringboot/backend/springbootback/src/test/java/com/example/springbootback/SpringbootbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
